882d11a59f2792a26b4bd0ac38ea1bc2cc844fdf
Brock5555 <77067332+Brock5555@users.noreply.github.com>
2021-05-09 15:05:34 +0530

    violet: Add custom CPU temp path
    Signed-off-by: Joker-V2 <nipinna0@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,6 +120,7 @@ TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/virtual/thermal/thermal_zone18/temp"
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXTRA_LANGUAGES := true
 TW_INCLUDE_NTFS_3G := true
