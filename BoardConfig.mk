df43d6aa1c56bc1e0cdb3962250c6d2b87f473c2
MD Raza <raza231198@gmail.com>
2022-09-28 21:37:32 +0530

    violet: Enable torch
    - Define node
    
    Signed-off-by: Joker-V2 <nipinna0@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,3 +155,6 @@ TW_INCLUDE_RESETPROP := true
 #TW_H_OFFSET := -80
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
+
+# Torch
+PB_TORCH_PATH := "/sys/class/leds/led:torch_0"
